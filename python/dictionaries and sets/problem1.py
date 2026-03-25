34adb6b048f32b202a7409e3b3800aa7cea4285a
Palak <palakgoyal22406@gmail.com>
2026-03-26 00:30:00 +0530

    python

---
+++ python/dictionaries and sets/problem1.py
@@ -0,0 +1,9 @@
+words ={
+    "madad":"help",
+    "kursi":"chair",
+    "chawal":"rice",
+    "chinni":"sugar"
+}
+
+word = input("enter the word you want to know meaning of: ")
+print(words[word])
